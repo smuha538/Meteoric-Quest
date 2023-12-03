e83528e7e5d8e3df13abbaf471a5c78e985cd0e3
Sahir Muhammad Tariq <59937362+smuha538@users.noreply.github.com>
2023-12-02 17:17:47 -0700

    final push (#12)

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 2f;
+    public Image boostBar;
+    public float maxBoost = 200f;
+    private float currentBoost;
     private Rigidbody2D rb;
+    private bool cooldown = false;
     private Vector2 movementDirection;
     // Start is called before the first frame update
     void Start()
     {
+        currentBoost = maxBoost;
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -21,14 +27,44 @@ public class ShipController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && currentBoost > 0 && cooldown == false)
         {
             rb.velocity = movementDirection * movementSpeed * 3;
+            ConsumeBoost(1);
         }
+        
         else
         {
             rb.velocity = movementDirection * movementSpeed;
+            if(currentBoost != maxBoost)
+            {
+                RechargeBoost(1);
+            }
+            
         }
             
+    }
+
+    public void ConsumeBoost(int boostUsed)
+    {
+        currentBoost -= boostUsed;
+        boostBar.fillAmount = currentBoost / maxBoost;
+ 
+        if (cooldown == false && currentBoost <= 0)
+        {
+            Invoke("BoostCooldown", 4.0f);
+            cooldown = true;
+        }
+    }
+
+    public void RechargeBoost(int boostRecharged)
+    {
+        currentBoost += boostRecharged;
+        boostBar.fillAmount = currentBoost / maxBoost;
+    }
+
+    void BoostCooldown()
+    {
+        cooldown = false;
     }
 }
